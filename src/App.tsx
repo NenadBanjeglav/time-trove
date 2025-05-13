7c63de8ef35584fefb4c80bd699a8809e6bd52a8
Nenad <nenadmobmail@gmail.com>
2025-05-13 10:58:50 +0200

    basic routing / favicon title/ theme.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import reactLogo from './assets/react.svg'
+import { AppLayout, PublicLayout, Dashboard, Login, Register, NotFound } from './pages'
 
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Protected App Routes */}
+        <Route
+          element={
+            // <ProtectedRoute>
+            <AppLayout />
+            // </ProtectedRoute>
+          }
+        >
+          <Route index element={<Dashboard />} />
+        </Route>
+
+        {/* Public Routes */}
+        <Route element={<PublicLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export const theme = {
+  colors: {
+    neutrals: {
+      0: '#FFFFFF',
+      50: '#F8F8F8',
+      100: '#DDE2E8',
+      200: '#A0A9B1',
+      300: '#5F6368',
+      400: '#2E3237',
+      500: '#000000',
+    },
+    system: {
+      success: {
+        0: '#FFFFFF',
+        50: '#4BE381',
+        100: '#2ED573',
+        200: '#1DB863',
+      },
+      warning: {
+        0: '#FFFFFF',
+        50: '#FFC266',
+        100: '#FFA940',
+        200: '#FF8C1A',
+      },
+      danger: {
+        0: '#FFFFFF',
+        50: '#FF6B6B',
+        100: '#FF4D4F',
+        200: '#D9363E',
+      },
+    },
+    primary: {
+      0: '#F5F0FF',
+      50: '#B197FC',
+      100: '#925FF0',
+      200: '#6F38D2',
+    },
+  },
+
+  typography: {
+    body: {
+      large: {
+        regular: { fontSize: '20px', lineHeight: '28px', fontWeight: 400 },
+        medium: { fontSize: '20px', lineHeight: '28px', fontWeight: 500 },
+        semiBold: { fontSize: '20px', lineHeight: '28px', fontWeight: 600 },
+        bold: { fontSize: '20px', lineHeight: '28px', fontWeight: 700 },
+      },
+      base: {
+        regular: { fontSize: '16px', lineHeight: '24px', fontWeight: 400 },
+        medium: { fontSize: '16px', lineHeight: '24px', fontWeight: 500 },
+        semiBold: { fontSize: '16px', lineHeight: '24px', fontWeight: 600 },
+        bold: { fontSize: '16px', lineHeight: '24px', fontWeight: 700 },
+      },
+      small: {
+        regular: { fontSize: '14px', lineHeight: '20px', fontWeight: 400 },
+        medium: { fontSize: '14px', lineHeight: '20px', fontWeight: 500 },
+        semiBold: { fontSize: '14px', lineHeight: '20px', fontWeight: 600 },
+        bold: { fontSize: '14px', lineHeight: '20px', fontWeight: 700 },
+      },
+      xSmall: {
+        regular: { fontSize: '12px', lineHeight: '16px', fontWeight: 400 },
+        medium: { fontSize: '12px', lineHeight: '16px', fontWeight: 500 },
+        semiBold: { fontSize: '12px', lineHeight: '16px', fontWeight: 600 },
+        bold: { fontSize: '12px', lineHeight: '16px', fontWeight: 700 },
+      },
+    },
+    heading: {
+      h1: {
+        regular: { fontSize: '32px', lineHeight: '40px', fontWeight: 400 },
+        medium: { fontSize: '32px', lineHeight: '40px', fontWeight: 500 },
+        semiBold: { fontSize: '32px', lineHeight: '40px', fontWeight: 600 },
+        bold: { fontSize: '32px', lineHeight: '40px', fontWeight: 700 },
+      },
+      h2: {
+        regular: { fontSize: '24px', lineHeight: '32px', fontWeight: 400 },
+        medium: { fontSize: '24px', lineHeight: '32px', fontWeight: 500 },
+        semiBold: { fontSize: '24px', lineHeight: '32px', fontWeight: 600 },
+        bold: { fontSize: '24px', lineHeight: '32px', fontWeight: 700 },
+      },
+    },
+  },
+}
