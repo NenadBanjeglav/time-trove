244631aafc053e6f779a0d55901de3236eee1f59
Nenad Banjeglav <nenadmobmail@gmail.com>
2025-05-16 09:28:14 +0200

    pallet and color for HeadingComponent

--- src/components/atoms/heading/Heading.tsx
+++ src/components/atoms/heading/Heading.tsx
@@ -1,26 +1,44 @@
-import type { FontSizeType, FontWeightType, LineHeightType } from '../../../styles/theme.types'
-import { StyledHeading } from './heading.styles'
+import type {
+  Color,
+  FontSizeType,
+  FontWeightType,
+  LineHeightType,
+  Pallete,
+} from '../../../styles/theme.types'
+import { createStyledHeading } from './heading.styles'
 
 type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
-type HeadingProps = {
+type HeadingProps<T extends Pallete = 'neutral'> = {
   as?: HeadingLevel
   fontSize?: FontSizeType
   lineHeight?: LineHeightType
   fontWeight?: FontWeightType
+  pallete?: T
+  color?: Color<T>
   children: React.ReactNode
   className?: string
 }
 
-export const Heading: React.FC<HeadingProps> = ({
+export const Heading = <T extends Pallete = 'neutral'>({
   as = 'h2',
   fontSize = 'h2',
   lineHeight = 'h2',
   fontWeight = 'bold',
-  children,
-}) => {
+  pallete = 'neutral' as T,
+  color = 'hue500' as Color<T>,
+  children = 'Random Heading',
+}: HeadingProps<T>) => {
+  const StyledHeading = createStyledHeading<T>()
   return (
-    <StyledHeading as={as} $fontSize={fontSize} $lineHeight={lineHeight} $fontWeight={fontWeight}>
+    <StyledHeading
+      as={as}
+      $fontSize={fontSize}
+      $lineHeight={lineHeight}
+      $fontWeight={fontWeight}
+      $pallete={pallete}
+      $color={color}
+    >
       {children}
     </StyledHeading>
   )
--- src/components/atoms/heading/heading.styles.ts
+++ src/components/atoms/heading/heading.styles.ts
@@ -1,15 +1,29 @@
 import styled from 'styled-components'
-import type { FontSizeType, FontWeightType, LineHeightType } from '../../../styles/theme.types'
+import type {
+  Color,
+  FontSizeType,
+  FontWeightType,
+  LineHeightType,
+  Pallete,
+} from '../../../styles/theme.types'
 
-type StyledProps = {
+type StyledProps<T extends Pallete = Pallete> = {
   $fontSize: FontSizeType
   $fontWeight: FontWeightType
   $lineHeight: LineHeightType
+  $pallete: T
+  $color: Color<T>
 }
 
-export const StyledHeading = styled.h1<StyledProps>`
-  font-size: ${({ theme, $fontSize }) => theme.typography.fontSize[$fontSize]};
-  font-weight: ${({ theme, $fontWeight }) => theme.typography.fontWeight[$fontWeight]};
-  line-height: ${({ theme, $lineHeight }) => theme.typography.lineHeight[$lineHeight]};
-  margin: 0;
-`
+export function createStyledHeading<T extends Pallete>() {
+  return styled.h1.withConfig({
+    shouldForwardProp: prop =>
+      !['$fontSize', '$fontWeight', '$lineHeight', '$pallete', '$color'].includes(prop),
+  })<StyledProps<T>>`
+    font-size: ${({ theme, $fontSize }) => theme.typography.fontSize[$fontSize]};
+    font-weight: ${({ theme, $fontWeight }) => theme.typography.fontWeight[$fontWeight]};
+    line-height: ${({ theme, $lineHeight }) => theme.typography.lineHeight[$lineHeight]};
+    color: ${({ theme, $pallete, $color }) => theme.colors[$pallete][$color] as string};
+    margin: 0;
+  `
+}
