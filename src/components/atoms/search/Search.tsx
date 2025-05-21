dd9ed5b3104aa7d9957627c63b59fb704188d631
Nenad Banjeglav <nenadmobmail@gmail.com>
2025-05-21 10:16:46 +0200

    debounce search params

--- src/components/atoms/search/Search.tsx
+++ src/components/atoms/search/Search.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { RemoveIcon } from '../../../assets/icons/RemoveIcon'
 import { SearchIcon } from '../../../assets/icons/SearchIcon'
 import { Icon } from '../icon/Icon'
@@ -10,13 +11,24 @@ type SearchProps = {
 
 export const Search = ({ placeholder = 'Search' }: SearchProps) => {
   const [searchParams, setSearchParams] = useSearchParams()
+  const currentSearch = searchParams.get('search')
+  const [search, setSearch] = useState(currentSearch || '')
 
-  const currentSearch = searchParams.get('search') || ''
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (search) {
+        searchParams.set('search', search)
+      } else {
+        searchParams.delete('search')
+      }
+      setSearchParams(searchParams)
+    }, 300)
+
+    return () => clearTimeout(timeout)
+  }, [search, searchParams, setSearchParams])
 
   const handleChange = (value: string) => {
-    if (value) searchParams.set('search', value)
-    else searchParams.delete('search')
-    setSearchParams(searchParams)
+    setSearch(value)
   }
 
   return (
@@ -26,11 +38,11 @@ export const Search = ({ placeholder = 'Search' }: SearchProps) => {
       </IconWrapper>
       <StyledInput
         type="text"
-        value={currentSearch}
+        value={search}
         onChange={e => handleChange(e.target.value)}
         placeholder={placeholder}
       />
-      {currentSearch && (
+      {search && (
         <ClearButton onClick={() => handleChange('')}>
           <Icon icon={RemoveIcon} pallete="neutral" color="hue300" iconSize="small" />
         </ClearButton>
