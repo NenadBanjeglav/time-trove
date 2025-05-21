37296353f0f220a574eeccf64c021075efc073c0
Nenad Banjeglav <nenadmobmail@gmail.com>
2025-05-21 11:53:17 +0200

    border color

--- src/components/atoms/card/card.styles.ts
+++ src/components/atoms/card/card.styles.ts
@@ -4,6 +4,7 @@ export const StyledCard = styled.div`
   background-color: ${({ theme }) => theme.colors.neutral.hue0};
   padding: ${({ theme }) => theme.spacing.medium};
   border-radius: ${({ theme }) => theme.borderRadius.xLarge};
+  border: 1px solid ${({ theme }) => theme.colors.neutral.hue100};
   width: fit-content;
   display: flex;
   flex-direction: column;
