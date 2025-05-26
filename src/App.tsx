2d8da9a5216a86d812c6c3e9e969facc3ce854e7
Nenad <nenadmobmail@gmail.com>
2025-05-26 14:46:30 +0200

    merge main into login signtup

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { NotFound } from './pages/NotFound'
 import { ProtectedRoute } from './pages/ProtectedRoute'
 import { PublicLayout } from './pages/PublicLayout'
 import { Register } from './pages/Register'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +17,6 @@ const queryClient = new QueryClient({
     },
   },
 })
-import { Signup } from './pages/Signup'
 
 const App = () => {
   return (
@@ -35,13 +33,14 @@ const App = () => {
             <Route index element={<Dashboard />} />
           </Route>
 
-        <Route element={<PublicLayout />}>
-          <Route path={ROUTES.login} element={<Login />} />
-          <Route path={ROUTES.register} element={<Register />} />
-          <Route path={ROUTES.notFound} element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route element={<PublicLayout />}>
+            <Route path={ROUTES.login} element={<Login />} />
+            <Route path={ROUTES.register} element={<Register />} />
+            <Route path={ROUTES.notFound} element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
