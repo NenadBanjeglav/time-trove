9f1e3e37a71835a85604e79ecb077afb839af04e
Nenad <nenadmobmail@gmail.com>
2025-06-05 10:30:52 +0200

    overlay

--- src/components/atoms/modal/modal.variants.ts
+++ src/components/atoms/modal/modal.variants.ts
@@ -24,5 +24,8 @@ export const modalVariants = {
 export const overlayVariants = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
-  exit: { opacity: 0 },
+  exit: {
+    opacity: 0,
+    transition: { delay: 0.2 },
+  },
 }
